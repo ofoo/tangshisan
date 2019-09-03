fb92dfdb1fe7eaaf13d13f4a98e0cd71ef3d7d8f
唐十三 <746790793@qq.com>
2019-09-03 11:00:37 +0800

    提交代码

---
+++ src/main/java/com/guoguo/TangshisanApplication.java
@@ -0,0 +1,13 @@
+package com.guoguo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TangshisanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TangshisanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/guoguo/tangshisan/TangshisanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.guoguo.tangshisan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TangshisanApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
